2ea4bf8640d9bbf3106dd9709dc0cf6253fec187
ezkahan <ezkahan.dev@gmail.com>
2023-01-21 03:11:52 +0500

    entities done

---
+++ src/modules/image/entities/image.go
@@ -0,0 +1,13 @@
+package entities
+
+import (
+	"gorm.io/gorm"
+)
+
+type Image struct {
+	gorm.Model
+	ID     uint64 `gorm:"primaryKey;unique;autoIncrement"`
+	BookID uint64
+	Image  string `gorm:"size:255" json:"image,omitempty"`
+	Type   string `gorm:"size:10" json:"type"`
+}
